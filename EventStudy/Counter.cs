b2b130edf593cf0077fe976be971c16e421e78bc
shahrukhkhan00677 <shahrukhkhan00677@gmail.com>
2023-08-01 09:12:33 +0530

    Event project added

---
+++ EventStudy/Counter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventStudy
+{
+    internal class Counter
+    {
+         int CurrentScore { get; set; }
+         int TargetScore { get; set; }
+
+        public Counter(int a)
+        {
+            CurrentScore = 0;
+            TargetScore = a;
+            
+        }
+        public void Add(int b)
+        {
+            //CurrentScore = CurrentScore + b;
+             CurrentScore += b;
+            Console.WriteLine(CurrentScore);
+
+            if(CurrentScore>=TargetScore)
+            {
+                Console.WriteLine("Win");
+            }
+            
+        }
+
+    }
+}
---
+++ EventStudy/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EventStudy;
+
+Console.WriteLine("Hello, World!");
+
+Counter obj = new Counter(20);
+obj.Add(6);
+obj.Add(4);
+obj.Add(6);
+obj.Add(6);
+
