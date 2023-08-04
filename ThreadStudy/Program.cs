0be002f6d825fe681ef0d30afcd6c18233071fba
shahrukhkhan00677 <shahrukhkhan00677@gmail.com>
2023-08-04 08:44:36 +0530

    thread project

---
+++ ThreadStudy/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ThreadStudy;
+
+Console.WriteLine("Thread Study");
+
+Student objStudent=new Student();
+//objStudent.Work1();
+//objStudent.Work2();
+
+ThreadStart s1 = new ThreadStart(objStudent.Work1);
+
+Thread thread1 = new Thread(s1);
+thread1.Name = "my thread";
+
+ThreadStart s2 = new ThreadStart(objStudent.Work2);
+
+Thread thread2 = new Thread(s2);
+thread2.Name = "my thread2";
+
+thread2.Start();
+thread1.Start();
---
+++ ThreadStudy/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadStudy
+{
+    internal class Student
+    {
+        public void Work1()
+        {
+            Console.WriteLine("Work1 called");
+
+        }
+
+        public void Work2()
+        {
+            Console.WriteLine("Work2 called");
+
+        }
+    }
+}
